0541560b05fe2ac3ed2a551123799bc83a60924d
Jongchae Jung <jcjung@alticast.com>
2022-12-29 19:33:51 -0700

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by jcjung on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
